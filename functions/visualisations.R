d310ffdbcf8200a2082df762d721933c77957c0a
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-08-20 15:44:16 +0200

    updates

---
+++ functions/visualisations.R
@@ -0,0 +1,60 @@
+BOX_CONTEXT_A <- list(x = c(10, 410), y = c(80, 340))
+BOX_CONTEXT_B <- list(x = c(0, 420), y = c(0, 420))
+
+paper_heatmap <- function(data, arena){
+  box <- ifelse(arena == "A", BOX_CONTEXT_A, BOX_CONTEXT_B)
+  obj <- combine_all(data)
+  plt <- ggplot() +
+    stat_density_2d(
+      geom = "raster",
+      data = obj$position$data,
+      aes(x=position_x, y = position_y, fill = after_stat(density)),
+      contour = FALSE
+    ) +
+    gradient_style() +
+    #lims(x = box$x, y = box$y) +
+    coord_fixed(xlim = box$x, ylim = box$y) +
+    theme_bw() +
+    heatmap_theme() +
+    labs(x = "",y = "") +
+    guides(fill = "none") +
+    #geom_point(data = obj$position$data, mapping = aes(position_x, position_y)) +
+    #geom_box_room() +
+    theme(panel.background = element_rect(fill = "transparent"),
+          axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank())
+  return(plt) 
+}
+
+#' Styling functions for the heatmap and paths
+gradient_style <- function(){
+  return(scale_fill_gradientn(colours = heatmap_color()))
+}
+
+heatmap_theme <- function(){
+  return(theme(panel.background = element_rect(fill = heatmap_color()[1]),
+               panel.grid.major = element_blank(),
+               panel.grid.minor = element_blank(),
+               panel.border = element_blank()))
+}
+
+heatmap_color <- function(){
+  return(rev(rainbow(100, start=0, end=0.7)))
+}
+
+area_presence_scale <- function(darkside){
+  presence_colors <- c("#e2e2e2", "#000000")
+  presence_colors <- setNames(presence_colors, 
+                              c(other_side_name(darkside), darkside))
+  return(presence_colors)
+}
+
+geom_box_room_local <- function(box, color = "grey20", size = 1.25,
+                                fill = "white", ...){
+  return(geom_rect(aes(xmin=box$x[1], xmax=box$x[2],
+                       ymin=box$y[1], ymax=box$y[2]),
+                   color = color, size = size, fill=fill, ...))
+}
---
+++ playground.R
@@ -0,0 +1,114 @@
+library(fgu.avoidance)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+anim_arena <- load_data("data/OTTER_raw_runs 43_44_final_normative_data/arena size_contextB.CSV")
+anim_dead <- load_data("data/OTTER_raw_runs 43_44_final_normative_data/blue_box_freezing_imtation of dead animal.CSV")
+anim_round <- load_data("data/OTTER_raw_runs 43_44_final_normative_data/round arena size_contextA.CSV")
+
+plot_path(anim_round$animal_8888)
+
+data_pth <- "data/OTTER_raw_runs 43_44_final_normative_data/"
+folders <- list.dirs(data_pth, full.names = FALSE, recursive = FALSE)
+
+## Table prep ------
+df_request <- as.data.frame(str_split(folders, "-", simplify = TRUE))
+df_request <- df_request %>%
+  mutate(across(everything(), str_trim)) %>%
+  separate(V1, into=c("condition", "group"), sep = "\\s+") %>%
+  separate(V3, into=c("analysis_1", "analysis_2"), sep="\\s\\+\\s",
+           fill="left") %>%
+  mutate(folder = list.dirs(data_pth, recursive = FALSE),
+         average_heatmap = grepl("average heatmap", analysis_2),
+         individual_analysis = !is.na(analysis_1))%>%
+  unite(code, condition, group, remove=FALSE)
+
+
+files <- list.files(data_pth, recursive = TRUE, full.names = TRUE)
+files <- files[grepl("//", files)]
+files <- gsub(data_pth, "", files)
+
+df_files <- as.data.frame(str_split(files, "/", simplify = TRUE)) %>%
+  select(-V1, folder = V2, file = V3) %>%
+  mutate(filename = gsub(".CSV", "", file)) %>%
+  separate(filename, into = c("run", "trial", "animal", "context"),
+           sep = "_", remove = TRUE) %>%
+  mutate(animal = gsub("rat[0]*", "", animal),
+         context = gsub("context", "", context),
+         run = gsub("run", "", run))
+
+## 
+anim_folder1 <- load_folder(df_request[1, "folder"])
+names(anim_folder1)
+
+session_results(anim_folder1$animal_1)
+collect_crosses(anim_folder1$animal_1)
+
+collect_crosses(anim_folder1)
+create_heatmap(anim_folder1)
+
+## Loading all -----
+all_data <- list()
+for(i in 1:nrow(df_request)){
+  line <- df_request[i,]
+  dat <- load_folder(line$folder)
+  all_data[[line$code]] <- dat
+}
+
+session_results(all_data$hab1_all)
+
+codingtables <- list()
+for(foldercode in names(all_data)){
+  if("codingtable" %in% names(all_data[[foldercode]])){
+    all_data[[foldercode]]$codingtable <- NULL
+  }
+  dat <- all_data[[foldercode]]
+  out <- sapply(names(dat), function(x){return(list(animalcode = x,
+                                             source = dat[[x]]$filepath))},
+              simplify = FALSE, USE.NAMES = TRUE)
+  codingtable <- data.frame(matrix(unlist(out),
+                                    nrow = length(out),
+                                    byrow = TRUE),
+                             stringsAsFactors = FALSE)
+  colnames(codingtable) <- c("animal", "source")
+  codingtable <- codingtable %>%
+    mutate(source = gsub(data_pth, "", source)) %>%
+    separate(source, into = c("trail", "folder", "file"), sep = "/", remove = FALSE) %>%
+    select(-trail)
+  codingtables[[foldercode]]$codingtable <- codingtable
+}
+
+
+
+## INDIVIDUAL results ----
+df_individual_results <- data.frame()
+for(i in 1:nrow(df_request)){
+  line <- df_request[i,]
+  if(line$individual_analysis) next
+  out <- session_results(all_data[[line$code]]) %>%
+    left_join(codingtables[[line$code]]$codingtable, by="animal") %>%
+    left_join(df_files, by=c("folder", "file")) %>%
+    select(-c(source, folder, file))
+  df_individual_results <- rbind(df_individual_results, out)
+}
+
+df_individual_results
+
+## Graphs ------
+
+for(i in 1:nrow(df_request)){
+  line <- df_request[i,]
+  temp <- codingtables$hab1_all$codingtable %>%
+    left_join(df_files, by=c("folder", "file")) %>%
+    select(-c(source, folder, file))
+  
+  contextA <- filter_animals(all_data[[line$code]], temp$animal.x[temp$context == "A"])
+  contextB <- filter_animals(all_data[[line$code]], temp$animal.x[temp$context == "B"])
+
+  print(paper_heatmap(contextA, "A") + ggtitle(line$code, subtitle = "Context A"))
+  print(paper_heatmap(contextB, "B") + ggtitle(line$code, subtitle = "Context B"))
+}
+
+plot_area_presence(contextA)
